6198fea6215186862daed9ca991ac7434fe5015f
IuliiaSelivanova <yulya.danilina.96@inbox.ru>
2025-03-26 22:12:42 +0300

    добавлены схемы в  models

---
+++ models/Categories.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Category", CategorySchema);
---
+++ models/User.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("User", UserSchema);
